5913ac899a6c189efcc781ab727908e741782193
liyu <2279402751@qq.com>
2023-07-21 20:06:21 +0800

    idea commit6-hot-fix

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -4,5 +4,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git6");
+        System.out.println("hello hot-fix");
     }
 }
